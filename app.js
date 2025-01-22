45b0dceba989a815b1be109f302c58ff094ecff4
Antonio Quiroz <antoniojavierquiroz@gmail.com>
2025-01-22 15:15:07 -0600

    Agregando titulo y parrafo con app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+let titulo = document.querySelector('h1'); // De esta manera conectamos los elementos de nuestro HTMl con nuestro Js 
+titulo.innerHTML = 'Juego del numero secreto';
+
+let parrafo = document.querySelector('p');
+parrafo.innerHTML = 'Indica un nuemero del 1 al 10';
+
+/*
+    * Para esto tambien podemos hacerlo por clases pero debido a que solo tenemos un parrafo y un titulo no hay necesidad de mas 
+    * 
+*/
